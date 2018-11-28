8d83e0820e53ffb04809f22dec7857a2a9295bb1
Carlos Henrique <carlaoss.h88@gmail.com>
2018-11-28 00:50:48 -0200

    Add files via upload

---
+++ loginLoja/assets/js/geral.js
@@ -0,0 +1,7 @@
+var email = document.getElementById("txtEmail");
+
+
+function exibeMSG() {
+    var email = document.getElementById("txtEmail");
+    alert("Mensagem enviada para o e-mail: "+email.value+ " ! Por favor, cheque seu e-mail!");
+}
---
+++ loginLoja/index.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>La Viana's - Login</title>
+		<link rel="stylesheet" href="assets/css/geral.css">
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+		<link rel="icon" href="imgs/logo-icon.png" type="image/x-icon">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+		<script src="assets/js/geral.js"></script>
+	</head>
+	<body>
+		
+	<div class="bloco-login">
+		<div class="head-login">
+			<img src="imgs/logo.png" alt="">
+		</div>
+		<div class="container-login">
+			Insira seu e-mail<br>
+			<input id="txtEmail" type="text"> <i class="fas fa-question-circle" title="Caso o e-mail inserido exista em nosso banco de dados, você receberá um link de como proceder para redefinir sua senha."></i><br>
+			<button type="submit" onclick="exibeMSG()">Enviar</button>
+		</div>
+	</div>
+
+	</body>
+</html>
